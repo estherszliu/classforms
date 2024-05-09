85628afc4fac16ba93075e6aa8da1d3e4b1416e6
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-09 19:50:41 +1000

    feat: parent structure in place

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-
-
-import logo from './logo.svg';
 import './App.css';
-import SocialCardForm from './components/SocialCardForm';
+import SocialCardParent from './components/SocialCardParent';
 
 function App() {
   return (
     <div className="App">
       
-      <SocialCardForm />
+      <SocialCardParent />
     </div>
   );
 }
 
 export default App;
-
